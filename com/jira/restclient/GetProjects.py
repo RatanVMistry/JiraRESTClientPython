83ea55753a35e3419fc22aa286e7140a119597a1
ratmistr <ratan.mistry30@hmail.com>
2019-06-11 11:27:50 -0700

    add print methods

--- com/jira/restclient/GetProjects.py
+++ com/jira/restclient/GetProjects.py
@@ -19,23 +19,34 @@ class GetProjects:
         util = Utils()
         res = util.restGETCall(path)
         #res = requests.get("http://" + endpoint +"/rest/api/2/project", auth=HTTPBasicAuth(username, password), verify=False)
-
+        '''
         for i in range(len(res.json())):
 
             print("ID = {id} -> KEY = {key} ->  NAME = {name}".format(id = res.json()[i]['id'],key = res.json()[i]['key'], name = res.json()[i]['name']))
+        '''
+        return res.json()
 
     def getProjectById(self, projectID):
         path = "/rest/api/2/project/{id}".format(id=projectID)
         util = Utils()
         res = util.restGETCall(path)
+        return res.json()
         #print(res.json())
         #res = requests.get("http://" + endpoint +"/rest/api/2/project", auth=HTTPBasicAuth(username, password), verify=False)
         print("ID = {id} -> KEY = {key} ->  NAME = {name}".format(id = res.json()['id'],key = res.json()['key'], name = res.json()['name']))
 
+    def printProjects(self, projectList):
+        for i in range(len(projectList)):
+            print("ID = {id} -> KEY = {key} ->  NAME = {name}".format(id=projectList[i]['id'], key=projectList[i]['key'], name=projectList[i]['name']))
 
-#project = GetProjects()
-#project.getProjects()
+    def printProjectByID(self,projectDetail):
+        print("ID = {id} -> KEY = {key} ->  NAME = {name}".format(id=projectDetail['id'], key=projectDetail['key'], name=projectDetail['name']))
+
+project = GetProjects()
+projectList = project.getProjects()
+project.printProjects(projectList)
 
 #getProjects()
-#projectID = input("Select Project ID = ")
-#project.getProjectById(projectID)
+projectID = input("Select Project ID = ")
+projectDetail = project.getProjectById(projectID)
+project.printProjectByID(projectDetail)
