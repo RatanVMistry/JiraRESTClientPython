dfc3bfaeac64061ecab88e649e12f46dd478c040
ratmistr <ratan.mistry30@hmail.com>
2019-06-13 13:28:41 -0700

    changed auth from auth.json file and cheange Application.py

--- com/jira/restclient/Application.py
+++ com/jira/restclient/Application.py
@@ -8,42 +8,73 @@ def selectOptaion():
     op = int(input("Enter number : "))
     #return op
     if(op == 1):
+
         getProject = GetProjects()
         projects = getProject.getProjects()
         getProject.printProjects(projects)
-        projectID= input("Select project for detail : ")
-        if projectID in projects:
-            projectDetail = getProject.getProjectById(projectID)
-            getProject.printProjectByID(projectDetail)
+
+        if len(projects) != 0 :
+            projectID= input("Select project for detail : ")
+            projectsIDList = []
+            for i in range(len(projects)):
+                projectsIDList.append(projects[i]['id'])
+            if projectID in projectsIDList:
+        #if any(projectID in d for d in projects):
+                projectDetail = getProject.getProjectById(projectID)
+                getProject.printProjectByID(projectDetail)
+            else:
+                print("You entred wrong project ID")
+
         else:
-            print("There is no project Id in list")
+            print("There is no project in JIRA")
+
+
     else:
 
         getProject = GetProjects()
         #getIssues = GetIssueFromProject()
         projects = getProject.getProjects()
-        print(projects)
+
         getProject.printProjects(projects)
-        projectID = int(input("Select project for issue in that projet  : "))
 
-        t = True
-        while t == True:
-            getIssueFromProjecrt = GetIssueFromProject()
-            issueList = getIssueFromProjecrt.getIssueFromProject(projectID)
-            if (len(issueList) > 0):
-                getIssueFromProjecrt.printIssuesFromProject(issueList)
-                issueID = input("Select issue ID : ")
-                issue = getIssueFromProjecrt.getIssueDetails(issueID)
-                getIssueFromProjecrt.printIssueDetails(issue)
-                t1 = input("Do you want another issue detail ?? Y/N ")
-                if (t1.lower() == 'n'):
-                    t = False
+        if len(projects) != 0:
+            projectID = input("Select project for issue in that projet  : ")
+            projectsIDList = []
+            for i in range(len(projects)):
+                projectsIDList.append(projects[i]['id'])
+
+            if projectID in projectsIDList:
+                t = True
+                while t == True:
+                    getIssueFromProjecrt = GetIssueFromProject()
+                    issueList = getIssueFromProjecrt.getIssueFromProject(projectID)
+
+                    if len(issueList) > 0:
+                        getIssueFromProjecrt.printIssuesFromProject(issueList)
+                        issueID = input("Select issue ID : ")
+                        issueIdList = []
+                        for i in range(len(issueList)):
+                            issueIdList.append(issueList[i]['id'])
+                        if issueID in issueIdList:
+                            issue = getIssueFromProjecrt.getIssueDetails(issueID)
+                            getIssueFromProjecrt.printIssueDetails(issue)
+                            t1 = input("Do you want another issue detail ?? Y/N ")
+                            if (t1.lower() == 'n'):
+                                t = False
+                        else:
+                            print("wrong Issue ID please enter correct one ")
+                            t1 = input("Do you want another issue detail ?? Y/N ")
+                            if (t1.lower() == 'n'):
+                                t = False
+                    else:
+                        print("There is no issue in this project")
+                        t = False
             else:
-                print("There is no issue in this project")
-                t = False
+                print("No project for this ID")
+        else:
+            print("There is no project in JIRA")
 
 
 
-
-        print("Done")
+    print("Done")
 selectOptaion()
--- com/jira/restclient/Utils.py
+++ com/jira/restclient/Utils.py
@@ -7,9 +7,23 @@ from requests.auth import HTTPBasicAuth
 class Utils:
 
     def __init__(self):
-        self.username= "admin"
-        self.password = "Welcome2cliqr!"
-        self.endpoint= "35.227.179.83:8080"
+        f = open("/Users/ratmistr/JiraRestClient/com/jira/restclient/auth.json", 'r')
+        try:
+            data = json.load(f)
+            #return data
+        except ValueError as e:
+            print
+            "Incorrect json format"
+            print
+            e
+            exit()
+        finally:
+            f.close()
+        self.username= data['username']
+        self.password = data['password']
+        self.endpoint= data['endPoint']
+
+
 
     def restGETCall(self, path):
         return requests.get("http://" + self.endpoint + path, auth=HTTPBasicAuth(self.username, self.password), verify=False)
